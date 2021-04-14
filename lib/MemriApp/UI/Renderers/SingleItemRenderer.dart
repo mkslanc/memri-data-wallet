52121b7078f338542b4394ac3b0d68da6f3a7207
mkslanc <mks-tech@ya.ru>
2021-04-14 16:12:41 +0300

    implemented SingleItemRenderer

--- lib/MemriApp/UI/Renderers/SingleItemRenderer.dart
+++ lib/MemriApp/UI/Renderers/SingleItemRenderer.dart
@@ -1,16 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:memri/MemriApp/Controllers/Database/ItemRecord.dart';
 import 'package:memri/MemriApp/Controllers/SceneController.dart';
+import 'package:memri/MemriApp/UI/CVUComponents/types/CVUColor.dart';
 
 import '../ViewContextController.dart';
 
 class SingleItemRendererView extends StatelessWidget {
   final SceneController sceneController;
   final ViewContextController viewContext;
+  late final EdgeInsets insets;
+  late final Color backgroundColor;
 
   SingleItemRendererView({required this.sceneController, required this.viewContext});
 
+  Future<bool> init() async {
+    insets = await viewContext.rendererDefinitionPropertyResolver.edgeInsets ?? EdgeInsets.zero;
+    backgroundColor = await viewContext.rendererDefinitionPropertyResolver.backgroundColor ??
+        CVUColor.system("systemBackground");
+    return true;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Expanded(child: Text("SingleItemRendererView"));
+    return FutureBuilder(
+        future: init(),
+        builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              return ValueListenableBuilder(
+                  valueListenable: viewContext.itemsValueNotifier,
+                  builder: (BuildContext context, List<ItemRecord> value, Widget? child) {
+                    var item = viewContext.focusedItem;
+                    if (item != null) {
+                      return Expanded(
+                          child: ColoredBox(
+                        color: backgroundColor,
+                        child: Padding(
+                          padding: insets,
+                          child: viewContext.render(item),
+                        ),
+                      ));
+                    } else {
+                      return Expanded(
+                          child: ColoredBox(
+                        color: backgroundColor,
+                        child: Padding(
+                          padding: insets,
+                          child: Center(
+                            child: Text("No item selected"),
+                          ),
+                        ),
+                      ));
+                    }
+                  });
+            default:
+              return SizedBox(
+                child: CircularProgressIndicator(),
+                width: 60,
+                height: 60,
+              );
+          }
+        });
   }
 }
