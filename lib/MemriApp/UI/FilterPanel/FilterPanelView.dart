74c4ffbf2f5ab50ee52759a198dddfd00c2181a5
mkslanc <mks-tech@ya.ru>
2021-04-27 17:22:06 +0400

    added OptionalDatePicker; fixed styling for FilterPanelView

--- lib/MemriApp/UI/FilterPanel/FilterPanelView.dart
+++ lib/MemriApp/UI/FilterPanel/FilterPanelView.dart
@@ -7,6 +7,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:memri/MemriApp/Helpers/Binding.dart';
+import 'package:memri/MemriApp/UI/CVUComponents/types/CVUColor.dart';
+import 'package:memri/MemriApp/UI/Components/OptionalDatePicker.dart';
 
 import '../ViewContextController.dart';
 
@@ -44,43 +46,87 @@ class _FilterPanelViewState extends State<FilterPanelView> {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
+    return Container(
       height: 340,
-      child: ColoredBox(
-        color: Color(0xffffffff),
-        child: ValueListenableBuilder(
-          builder: (BuildContext context, FilterPanelTab value, Widget? child) {
-            return Column(
-              children: [
-                Row(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Color.fromRGBO(0, 0, 0, 0.4),
+            blurRadius: 10,
+          )
+        ],
+        borderRadius:
+            BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+      ),
+      child: ValueListenableBuilder(
+        builder: (BuildContext context, FilterPanelTab value, Widget? child) {
+          return Column(
+            children: [
+              SizedBox(
+                height: 40,
+                child: Row(
                   children: [
                     tabButton(Icons.note, FilterPanelTab.renderer),
-                    Divider(),
+                    VerticalDivider(
+                      width: 1,
+                    ),
                     tabButton(Icons.settings, FilterPanelTab.rendererOptions),
-                    Divider(),
+                    VerticalDivider(
+                      width: 1,
+                    ),
                     tabButton(Icons.restore, FilterPanelTab.filterOptions),
-                    Divider()
+                    VerticalDivider(
+                      width: 1,
+                    )
                   ],
                 ),
-                Divider(),
-                Expanded(
-                    child: MaterialApp(
-                        debugShowCheckedModeBanner: false,
-                        home: Scaffold(
-                          appBar: AppBar(
-                            title: Center(
-                              child: Text(currentTabTitle),
-                            ),
+              ),
+              Divider(
+                height: 1,
+              ),
+              Expanded(
+                  child: MaterialApp(
+                      debugShowCheckedModeBanner: false,
+                      home: Scaffold(
+                        backgroundColor: Color(0xfff3f2f8),
+                        appBar: PreferredSize(
+                          preferredSize: Size.fromHeight(32.0),
+                          child: Column(
+                            children: [
+                              AppBar(
+                                centerTitle: true,
+                                primary: false,
+                                backgroundColor: Colors.white,
+                                excludeHeaderSemantics: true,
+                                title: Center(
+                                    child: Text(
+                                  currentTabTitle,
+                                  style: TextStyle(color: Colors.black, fontSize: 17),
+                                )),
+                              ),
+                            ],
                           ),
-                          body: Form(
-                            child: currentTabRenderer,
-                          ),
-                        )))
-              ],
-            );
-          },
-          valueListenable: _currentTab,
-        ),
+                        ),
+                        body: SingleChildScrollView(
+                          physics: BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
+                          child: Padding(
+                              padding: const EdgeInsets.all(20.0),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                                ),
+                                child: Form(
+                                  child: currentTabRenderer,
+                                ),
+                              )),
+                        ),
+                      )))
+            ],
+          );
+        },
+        valueListenable: _currentTab,
       ),
     );
   }
@@ -98,25 +144,29 @@ class _FilterPanelViewState extends State<FilterPanelView> {
 
   Widget tabButton(IconData icon, FilterPanelTab tab) {
     return Expanded(
-      child: ElevatedButton(
-        child: Icon(
-          icon,
-          color: Colors.black,
+      child: Container(
+        decoration: BoxDecoration(
+          color: currentTab == tab ? CVUColor.system("systemFill") : Colors.transparent,
         ),
-        // icon: ,
-        onPressed: () => currentTab = tab,
-        style: ElevatedButton.styleFrom(
-          padding: EdgeInsets.all(5),
-          primary: currentTab == tab ? Colors.grey : Colors.white,
-        ), //? Color(.systemFill) : Color(.secondarySystemBackground)
+        child: TextButton(
+          child: Icon(
+            icon,
+            color: Colors.black,
+          ),
+          // icon: ,
+          onPressed: () => currentTab = tab,
+          style: TextButton.styleFrom(
+            padding: EdgeInsets.all(5),
+            primary: currentTab == tab ? CVUColor.system("systemFill") : Colors.transparent,
+          ), //? Color(.systemFill) : Color(.secondarySystemBackground)
+        ),
       ),
     );
   }
 
   Widget optionalDateRow(String title, Binding<DateTime?> selection, [DateTime? initialSet]) {
     initialSet ??= DateTime.now();
-    return Text(initialSet.toString());
-    // OptionalDatePicker(title: title, selection: selection, initialSet: initialSet)
+    return OptionalDatePicker(title: title, selection: selection, initialSet: initialSet);
   }
 
   Widget get rendererTab {
@@ -130,19 +180,37 @@ class _FilterPanelViewState extends State<FilterPanelView> {
                 valueListenable: viewContext.config.rendererName,
                 builder: (BuildContext context, String selectedRendererName, Widget? child) =>
                     Center(
-                      child: Column(
-                          children: supportedRenderers
-                              .map((rendererName) => TextButton(
-                                  onPressed: () =>
-                                      viewContext.config.rendererName.value = rendererName,
-                                  child: Text(
-                                    rendererName.toUpperCase(),
-                                    style: TextStyle(
-                                        fontWeight: rendererName == selectedRendererName
-                                            ? FontWeight.bold
-                                            : null),
-                                  )))
-                              .toList()),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                        ),
+                        child: ListView.separated(
+                            padding: EdgeInsets.zero,
+                            shrinkWrap: true,
+                            itemBuilder: (context, index) => TextButton(
+                                  style: TextButton.styleFrom(
+                                    padding: EdgeInsets.zero,
+                                  ),
+                                  onPressed: () => viewContext.config.rendererName.value =
+                                      supportedRenderers[index],
+                                  child: ListTile(
+                                      dense: true,
+                                      minVerticalPadding: 0,
+                                      title: Text(
+                                        supportedRenderers[index].toUpperCase(),
+                                        style: TextStyle(
+                                            fontWeight:
+                                                supportedRenderers[index] == selectedRendererName
+                                                    ? FontWeight.bold
+                                                    : null),
+                                      )),
+                                ),
+                            separatorBuilder: (context, index) => Divider(
+                                  height: 0,
+                                ),
+                            itemCount: supportedRenderers.length),
+                      ),
                     ));
           } else {
             return Center(
@@ -165,33 +233,51 @@ class _FilterPanelViewState extends State<FilterPanelView> {
       case "grid":
       // return GridRendererSettingsView(viewContext: viewContext);
       default:
-        return Text("No configurable settings for this renderer.");
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("No configurable settings for this renderer.", style: TextStyle(fontSize: 16)),
+            ],
+          ),
+        );
     }
   }
 
   Widget get filterOptionsTab {
     var date = DateTime.now();
-    return Column(
-      children: [
-        optionalDateRow(
-            "Modified after",
-            Binding(() => viewContext.config.query.dateModifiedAfter,
-                (newValue) => viewContext.config.query.dateModifiedAfter = newValue),
-            date.subtract(Duration(days: 7))),
-        optionalDateRow(
-            "Modified before",
-            Binding(() => viewContext.config.query.dateModifiedBefore,
-                (newValue) => viewContext.config.query.dateModifiedBefore = newValue)),
-        optionalDateRow(
-            "Created after",
-            Binding(() => viewContext.config.query.dateModifiedAfter,
-                (newValue) => viewContext.config.query.dateModifiedAfter = newValue),
-            date.subtract(Duration(days: 7))),
-        optionalDateRow(
-            "Created before",
-            Binding(() => viewContext.config.query.dateCreatedBefore,
-                (newValue) => viewContext.config.query.dateCreatedBefore = newValue)),
-      ],
-    );
+    List<Widget> filterOptions = [
+      optionalDateRow(
+          "Modified after",
+          Binding(() => viewContext.config.query.dateModifiedAfter,
+              (newValue) => viewContext.config.query.dateModifiedAfter = newValue),
+          date.subtract(Duration(days: 7))),
+      optionalDateRow(
+          "Modified before",
+          Binding(() => viewContext.config.query.dateModifiedBefore,
+              (newValue) => viewContext.config.query.dateModifiedBefore = newValue)),
+      optionalDateRow(
+          "Created after",
+          Binding(() => viewContext.config.query.dateModifiedAfter,
+              (newValue) => viewContext.config.query.dateModifiedAfter = newValue),
+          date.subtract(Duration(days: 7))),
+      optionalDateRow(
+          "Created before",
+          Binding(() => viewContext.config.query.dateCreatedBefore,
+              (newValue) => viewContext.config.query.dateCreatedBefore = newValue)),
+    ];
+    return ListView.separated(
+        padding: EdgeInsets.zero,
+        shrinkWrap: true,
+        itemBuilder: (BuildContext context, int index) => ListTile(
+              dense: true,
+              minVerticalPadding: 0,
+              title: filterOptions[index],
+            ),
+        separatorBuilder: (context, index) => Divider(
+              height: 0,
+            ),
+        itemCount: filterOptions.length);
   }
 }
