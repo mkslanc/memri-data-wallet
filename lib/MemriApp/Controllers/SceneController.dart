6ba6250cd3691001f2419d354b4433c3a81c9e68
aniam <amirjanyanani@gmail.com>
2021-05-18 19:11:14 +0400

    toggleEditMode fixed

--- lib/MemriApp/Controllers/SceneController.dart
+++ lib/MemriApp/Controllers/SceneController.dart
@@ -48,12 +48,18 @@ class SceneController {
 
   toggleEditMode() {
     var topConfigHolder = topMostContext?.configHolder;
-    var viewArgs = topConfigHolder?.config.viewArguments;
-    if (topConfigHolder == null || viewArgs == null) {
+    if (topConfigHolder == null) {
       return;
     }
+    var viewArgs = topConfigHolder.config.viewArguments;
+    viewArgs ??= CVUViewArguments();
 
     isInEditMode.value = !isInEditMode.value;
+
+    if (!isInEditMode.value) {
+      // Clear selection when ending edit mode
+      topMostContext?.selectedItems = [];
+    }
 
     var currentArgs = viewArgs.args;
     currentArgs["readOnly"] = CVUValueConstant(CVUConstantBool(!isInEditMode.value));
