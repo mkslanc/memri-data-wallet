4f06dd4c7672526f8ae987269dc0e609ce1fe473
aniam <amirjanyanani@gmail.com>
2021-05-18 20:19:36 +0400

    selectionMode for isInEditMode fixed

--- lib/MemriApp/UI/Renderers/GridRenderer.dart
+++ lib/MemriApp/UI/Renderers/GridRenderer.dart
@@ -1,67 +1,136 @@
 import 'package:flutter/material.dart';
 import 'package:memri/MemriApp/Controllers/Database/ItemRecord.dart';
 import 'package:memri/MemriApp/Controllers/SceneController.dart';
+import 'package:memri/MemriApp/Helpers/Binding.dart';
+import 'package:memri/MemriApp/UI/Components/ShapesAndProgress/Circle.dart';
 import '../ViewContextController.dart';
 import 'package:memri/MemriApp/Extensions/BaseTypes/Collection.dart';
 
 /// The grid renderer
 /// This presents the data in a grid (aka collection view)
-class GridRendererView extends StatelessWidget {
+class GridRendererView extends StatefulWidget {
   final SceneController sceneController;
   final ViewContextController viewContext;
 
   GridRendererView({required this.sceneController, required this.viewContext});
 
-  late final bool isInEditMode;
+  @override
+  _GridRendererViewState createState() => _GridRendererViewState();
+}
+
+class _GridRendererViewState extends State<GridRendererView> {
+  late final SceneController sceneController;
+  late final ViewContextController viewContext;
+  late bool isInEditMode;
+
+  late Binding<Set<int>> selectedIndicesBinding;
+  late Set<int> selectedIndices;
+
+  late Future _init;
+
+  initState() {
+    super.initState();
+    sceneController = widget.sceneController;
+    viewContext = widget.viewContext;
+    _init = init();
+
+    sceneController.isInEditMode.addListener(updateIsInEditMode);
+  }
+
+  updateIsInEditMode() async {
+    await init();
+    setState(() {});
+  }
+
+  dispose() {
+    super.dispose();
+    sceneController.isInEditMode.removeListener(updateIsInEditMode);
+  }
 
   Future init() async {
     isInEditMode = (await viewContext.viewDefinitionPropertyResolver
         .boolean("editMode", sceneController.isInEditMode.value))!;
+
+    selectedIndicesBinding = viewContext.selectedIndicesBinding;
+    selectedIndices = selectedIndicesBinding.get();
   }
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: init(),
-      builder: (BuildContext context, AsyncSnapshot snapshot) =>
-          snapshot.connectionState == ConnectionState.done
-              ? ValueListenableBuilder(
-                  valueListenable: viewContext.itemsValueNotifier,
-                  builder: (BuildContext context, List<ItemRecord> value, Widget? child) {
-                    return viewContext.hasItems
-                        ? Expanded(
-                            child: GridView.count(
-                            physics: AlwaysScrollableScrollPhysics(),
-                            childAspectRatio: 4 / 5,
-                            shrinkWrap: true,
-                            primary: false,
-                            padding: const EdgeInsets.all(5),
-                            crossAxisSpacing: 5,
-                            mainAxisSpacing: 5,
-                            crossAxisCount: 3,
-                            children: viewContext.items
-                                .mapIndexed((index, item) => GestureDetector(
-                                      onTap: selectionMode(index),
-                                      child: Container(
-                                        alignment: Alignment.bottomRight,
-                                        child: viewContext.render(item: item),
+      future: _init,
+      builder: (BuildContext context, AsyncSnapshot snapshot) => snapshot.connectionState ==
+              ConnectionState.done
+          ? ValueListenableBuilder(
+              valueListenable: viewContext.itemsValueNotifier,
+              builder: (BuildContext context, List<ItemRecord> value, Widget? child) {
+                return viewContext.hasItems
+                    ? Expanded(
+                        child: GridView.count(
+                        physics: AlwaysScrollableScrollPhysics(),
+                        childAspectRatio: 4 / 5,
+                        shrinkWrap: true,
+                        primary: false,
+                        padding: const EdgeInsets.all(5),
+                        crossAxisSpacing: 5,
+                        mainAxisSpacing: 5,
+                        crossAxisCount: 3,
+                        children: viewContext.items.mapIndexed((index, item) {
+                          var isSelected = selectedIndices.contains(index);
+                          return GestureDetector(
+                            onTap: selectionMode(index),
+                            child: Stack(
+                              alignment: Alignment.bottomRight,
+                              children: [
+                                viewContext.render(item: item),
+                                if (isInEditMode && !isSelected)
+                                  SizedBox.expand(
+                                    child: ColoredBox(color: Colors.white.withOpacity(0.15)),
+                                  ),
+                                if (isSelected)
+                                  Padding(
+                                    padding: const EdgeInsets.all(20),
+                                    child: Container(
+                                      height: 30,
+                                      width: 30,
+                                      child: Stack(
+                                        alignment: Alignment.center,
+                                        children: [
+                                          Circle(
+                                              color: Colors.blue,
+                                              border: Border.all(color: Colors.white, width: 2)),
+                                          Icon(
+                                            Icons.check,
+                                            color: Colors.white,
+                                            size: 15,
+                                          )
+                                        ],
                                       ),
-                                    ))
-                                .toList(),
-                          ))
-                        : Row(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [],
+                                    ),
+                                  )
+                              ],
+                            ),
                           );
-                  })
-              : SizedBox.shrink(),
+                        }).toList(),
+                      ))
+                    : Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [],
+                      );
+              })
+          : SizedBox.shrink(),
     );
   }
 
-  GestureTapCallback selectionMode(index) {
+  selectionMode(index) {
     if (isInEditMode) {
       return () {
-        print(index); //TODO select
+        setState(() {
+          if (!selectedIndices.remove(index)) {
+            selectedIndices.add(index);
+          }
+          selectedIndicesBinding.set(selectedIndices);
+        });
       };
     } else {
       return () {
--- lib/MemriApp/UI/Renderers/ListRenderer.dart
+++ lib/MemriApp/UI/Renderers/ListRenderer.dart
@@ -1,8 +1,10 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:memri/MemriApp/CVU/actions/CVUAction.dart';
 import 'package:memri/MemriApp/Controllers/Database/ItemRecord.dart';
 import 'package:memri/MemriApp/Controllers/SceneController.dart';
+import 'package:memri/MemriApp/Helpers/Binding.dart';
 import 'package:memri/MemriApp/UI/CVUComponents/types/CVUColor.dart';
 
 import '../ViewContextController.dart';
@@ -16,14 +18,12 @@ class ListRendererView extends StatefulWidget {
   ListRendererView({required this.sceneController, required this.viewContext});
 
   @override
-  _ListRendererViewState createState() => _ListRendererViewState(sceneController, viewContext);
+  _ListRendererViewState createState() => _ListRendererViewState();
 }
 
 class _ListRendererViewState extends State<ListRendererView> {
-  final SceneController sceneController;
-  final ViewContextController viewContext;
-
-  _ListRendererViewState(this.sceneController, this.viewContext);
+  late final SceneController sceneController;
+  late final ViewContextController viewContext;
 
   late EdgeInsets insets;
   late Point spacing;
@@ -31,7 +31,32 @@ class _ListRendererViewState extends State<ListRendererView> {
   late bool separatorsEnabled;
   late bool isInEditMode;
 
-  Future<bool> init() async {
+  late Binding<Set<int>> selectedIndicesBinding;
+  late Set<int> selectedIndices;
+
+  late Future _init;
+
+  initState() {
+    super.initState();
+    sceneController = widget.sceneController;
+    viewContext = widget.viewContext;
+    _init = init();
+
+    sceneController.isInEditMode.addListener(updateIsInEditMode);
+  }
+
+  updateIsInEditMode() async {
+    isInEditMode = (await viewContext.viewDefinitionPropertyResolver
+        .boolean("editMode", sceneController.isInEditMode.value))!;
+    setState(() {});
+  }
+
+  dispose() {
+    super.dispose();
+    sceneController.isInEditMode.removeListener(updateIsInEditMode);
+  }
+
+  Future<void> init() async {
     insets = await viewContext.rendererDefinitionPropertyResolver.edgeInsets ??
         EdgeInsets.only(top: 10, left: 10, bottom: 10, right: 10);
     spacing = await viewContext.rendererDefinitionPropertyResolver.spacing ?? Point(10, 10);
@@ -43,78 +68,99 @@ class _ListRendererViewState extends State<ListRendererView> {
     isInEditMode = (await viewContext.viewDefinitionPropertyResolver
         .boolean("editMode", sceneController.isInEditMode.value))!; // sceneController.isInEditMode
 
-    return true;
+    selectedIndicesBinding = viewContext.selectedIndicesBinding;
+    selectedIndices = selectedIndicesBinding.get();
   }
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-        future: init(),
-        builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.done:
-              return ValueListenableBuilder(
-                  valueListenable: viewContext.itemsValueNotifier,
-                  builder: (BuildContext context, List<ItemRecord> value, Widget? child) {
-                    if (viewContext.hasItems) {
-                      return Expanded(
-                          child: ListView.separated(
-                              physics:
-                                  BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
-                              padding: EdgeInsets.fromLTRB(0, insets.top, 0, insets.bottom),
-                              itemBuilder: (context, index) => ListTile(
+      future: _init,
+      builder: (context, snapshot) => snapshot.connectionState == ConnectionState.done
+          ? ValueListenableBuilder(
+              valueListenable: viewContext.itemsValueNotifier,
+              builder: (BuildContext context, List<ItemRecord> value, Widget? child) {
+                if (value.isNotEmpty) {
+                  return Expanded(
+                      child: ListView.separated(
+                          physics: BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
+                          padding: EdgeInsets.fromLTRB(0, insets.top, 0, insets.bottom),
+                          itemBuilder: (context, index) {
+                            var item = value[index];
+                            var title = Padding(
+                                padding: EdgeInsets.fromLTRB(
+                                    insets.left,
+                                    index == 0 ? 0 : spacing.y / 2,
+                                    insets.right,
+                                    index == value.length - 1 ? 0 : spacing.y / 2),
+                                child: ColoredBox(
+                                  color: backgroundColor,
+                                  child: viewContext.render(item: item, key: Key(item.uid)),
+                                ));
+                            var callback = selectionMode(index);
+                            return isInEditMode
+                                ? CheckboxListTile(
                                     dense: true,
-                                    title: Padding(
-                                        padding: EdgeInsets.fromLTRB(
-                                            insets.left,
-                                            index == 0 ? 0 : spacing.y / 2,
-                                            insets.right,
-                                            index == viewContext.items.length - 1
-                                                ? 0
-                                                : spacing.y / 2),
-                                        child: ColoredBox(
-                                          color: backgroundColor,
-                                          child: viewContext.render(item: viewContext.items[index]),
-                                        )),
-                                    onTap: selectionMode(index),
-                                  ),
-                              separatorBuilder: (context, index) => Divider(
-                                    height: 1,
-                                    color: separatorsEnabled ? null : Colors.transparent,
-                                  ),
-                              itemCount: viewContext.items.length));
-                    } else {
-                      return Expanded(
-                          child: Padding(
-                        padding: EdgeInsets.all(30),
-                        child: Center(
-                          child: Text(
-                            "No items",
-                            style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.normal,
-                                color: Color.fromRGBO(0, 0, 0, 0.7),
-                                backgroundColor: backgroundColor),
-                          ),
-                        ),
-                      ));
-                    }
-                  });
-
-            default:
-              return SizedBox(
-                child: CircularProgressIndicator(),
-                width: 60,
-                height: 60,
-              );
-          }
-        });
+                                    title: title,
+                                    onChanged: callback,
+                                    value: selectedIndices.contains(index),
+                                    controlAffinity: ListTileControlAffinity.leading)
+                                : Dismissible(
+                                    direction: DismissDirection.endToStart,
+                                    key: Key(item.uid),
+                                    onDismissed: (direction) async {
+                                      var action = CVUActionDelete();
+                                      await action
+                                          .execute(sceneController,
+                                              viewContext.getCVUContext(item: item))
+                                          .then((value) => viewContext.setupQueryObservation());
+                                    },
+                                    child: ListTile(
+                                      dense: true,
+                                      title: title,
+                                      onTap: callback,
+                                    ),
+                                  );
+                          },
+                          separatorBuilder: (context, index) => Divider(
+                                height: 1,
+                                color: separatorsEnabled ? null : Colors.transparent,
+                              ),
+                          itemCount: value.length));
+                } else {
+                  return Expanded(
+                      child: Padding(
+                    padding: EdgeInsets.all(30),
+                    child: Center(
+                      child: Text(
+                        "No items",
+                        style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.normal,
+                            color: Color.fromRGBO(0, 0, 0, 0.7),
+                            backgroundColor: backgroundColor),
+                      ),
+                    ),
+                  ));
+                }
+              })
+          : SizedBox(
+              child: CircularProgressIndicator(),
+              width: 60,
+              height: 60,
+            ),
+    );
   }
 
-  GestureTapCallback selectionMode(index) {
+  selectionMode(index) {
     if (isInEditMode) {
-      return () {
-        print(index); //TODO select
+      return (bool? newValue) {
+        setState(() {
+          if (!selectedIndices.remove(index)) {
+            selectedIndices.add(index);
+          }
+          selectedIndicesBinding.set(selectedIndices);
+        });
       };
     } else {
       return () {
