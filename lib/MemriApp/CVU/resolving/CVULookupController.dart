0ca7534c741896f5c2021076002dc450a42e9af6
aniam <amirjanyanani@gmail.com>
2021-06-15 18:53:23 +0400

    Resolving currentIndex and items (from cvuContext).

--- lib/MemriApp/CVU/resolving/CVULookupController.dart
+++ lib/MemriApp/CVU/resolving/CVULookupController.dart
@@ -277,7 +277,10 @@ class CVULookupController {
         List<CVUExpressionNode> args = nodeType.args;
         switch (node.name.toLowerCase()) {
           case "item":
-            var exp = nodeType.args[0];
+            var exp = nodeType.args.asMap()[0];
+            if (exp == null) {
+              return null;
+            }
             int? itemRowId = await resolve<int>(expression: exp, context: context, db: db);
             if (itemRowId == null) {
               return null;
@@ -571,6 +574,14 @@ class CVULookupController {
             if (item != null && item.rowId != null) {
               currentValue = LookupStepValues([PropertyDatabaseValueInt(item.rowId!)]);
             }
+          } else if (node.name == "items") {
+            List<ItemRecord>? items = context.items;
+            if (items == null) {
+              return null;
+            }
+            currentValue = LookupStepItems(items);
+          } else if (node.name == "currentIndex") {
+            currentValue = LookupStepValues([PropertyDatabaseValueInt(context.currentIndex + 1)]);
           }
         } else {
           return null;
