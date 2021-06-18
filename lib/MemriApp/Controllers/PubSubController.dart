71a6d3c443bc69a3fee6068f3f8600097feead5d
mkslanc <mks-tech@ya.ru>
2021-06-18 18:34:03 +0400

    `PubSubController` implementation (with streams approach)

---
+++ lib/MemriApp/Controllers/PubSubController.dart
@@ -0,0 +1,122 @@
+import 'dart:async';
+
+import 'package:equatable/equatable.dart';
+import 'package:memri/MemriApp/Controllers/Database/DatabaseController.dart';
+import 'package:memri/MemriApp/Extensions/BaseTypes/Collection.dart';
+import 'package:memri/MemriApp/Controllers/Database/ItemRecord.dart';
+import 'package:moor/moor.dart';
+
+import 'AppController.dart';
+import 'Database/PropertyDatabaseValue.dart';
+
+class ItemSubscription with EquatableMixin {
+  ItemRecord item;
+  String property;
+  int retryCount;
+  PropertyDatabaseValue desiredValue;
+  Function(PropertyDatabaseValue?, [String?]) completion;
+  StreamSubscription? streamSubscription;
+
+  ItemSubscription(
+      {required this.item,
+      required this.property,
+      this.retryCount = 0,
+      required this.desiredValue,
+      required this.completion});
+
+  @override
+  List<Object?> get props => [item.uid, property];
+}
+
+class PubSubController {
+  static int pollInterval = 3;
+  static int maxRetryAttempts = 10;
+
+  Set<ItemSubscription> _subscribers = {};
+  Set<StreamSubscription<List<dynamic>>> _subscriptions = {};
+  DatabaseController databaseController;
+
+  PubSubController(this.databaseController);
+
+  startObservingItemProperty(
+      {required ItemRecord item,
+      required String property,
+      required PropertyDatabaseValue desiredValue,
+      required Function(PropertyDatabaseValue?, [String?]) completion}) {
+    var subscription = ItemSubscription(
+        item: item, property: property, desiredValue: desiredValue, completion: completion);
+    _subscribers.add(subscription);
+    _startObserver(subscription);
+  }
+
+  stopObservingItemProperty({required ItemRecord item, required String property}) {
+    var subscription = _subscriptionForItem(item: item, property: property);
+    if (subscription == null) {
+      return;
+    }
+    _cancelSubscription(subscription: subscription);
+  }
+
+  ItemSubscription? _subscriptionForItem({required ItemRecord item, required String property}) {
+    return _subscribers.firstWhereOrNull(
+        (element) => element.item.uid == item.uid && element.property == property);
+  }
+
+  _cancelSubscription({required ItemSubscription subscription, String? error}) {
+    subscription.completion(null, error ?? "Cancelled");
+    _subscribers.remove(subscription);
+    if (subscription.streamSubscription != null) {
+      subscription.streamSubscription?.cancel();
+      _subscriptions.remove(subscription.streamSubscription);
+    }
+  }
+
+  _checkSubscriptionFulfilled(
+      {required dynamic itemProperty, required ItemSubscription subscription}) {
+    var containsValue = _containsExpectedValue(
+        itemProperty: itemProperty,
+        itemType: subscription.item.type,
+        property: subscription.property,
+        expectedValue: subscription.desiredValue);
+    if (containsValue) {
+      subscription.completion(subscription.desiredValue);
+      stopObservingItemProperty(item: subscription.item, property: subscription.property);
+      return;
+    }
+  }
+
+  _startObserver(ItemSubscription subscription) {
+    var stream = databaseController.databasePool
+        .itemPropertyRecordsCustomSelectStream("name = ? AND value = ? AND item = ?", [
+      Variable(subscription.property),
+      Variable(subscription.desiredValue.value),
+      Variable(subscription.item.rowId)
+    ]);
+    var streamSubscription = stream.listen((List<dynamic> records) {
+      if (records.isNotEmpty) {
+        _checkSubscriptionFulfilled(itemProperty: records[0], subscription: subscription);
+      }
+    });
+    subscription.streamSubscription = streamSubscription;
+    _subscriptions.add(streamSubscription);
+  }
+
+  bool _containsExpectedValue(
+      {required dynamic itemProperty,
+      required String itemType,
+      required String property,
+      required PropertyDatabaseValue expectedValue}) {
+    var decodableValue = itemProperty.value;
+    if (decodableValue == null) {
+      return false;
+    }
+    var schema = AppController.shared.databaseController.schema;
+    var expectedType = schema.types[itemType]?.propertyTypes[property];
+    if (expectedType == null) {
+      return false;
+    }
+
+    var databaseValue = PropertyDatabaseValue.create(decodableValue, expectedType.valueType);
+    return databaseValue == expectedValue;
+  }
+}
---
+++ test/Database/PubSubController_test.dart
@@ -0,0 +1,54 @@
+import 'package:memri/MemriApp/Controllers/AppController.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:memri/MemriApp/Controllers/Database/ItemRecord.dart';
+import 'package:memri/MemriApp/Controllers/Database/PropertyDatabaseValue.dart';
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+  late AppController appController;
+  setUp(() async {
+    appController = AppController.shared;
+    await appController.databaseController.init();
+    await appController.databaseController.setupWithDemoData();
+  });
+
+  test('testPluginAuthorisationFlow', () async {
+    var record = ItemRecord(type: "StartPlugin");
+    await record.save();
+    await record.setPropertyValue("state", PropertyDatabaseValueString("idle"));
+    appController.pubsubController.startObservingItemProperty(
+        item: record,
+        property: "state",
+        desiredValue: PropertyDatabaseValueString("userActionNeeded"),
+        completion: (newValue, [error]) async {
+          if (newValue == null) {
+            return;
+          }
+          print("got `userActionNeeded`");
+          expect(newValue.value, equals("userActionNeeded"));
+
+          appController.pubsubController.startObservingItemProperty(
+              item: record,
+              property: "state",
+              desiredValue: PropertyDatabaseValueString("ready"),
+              completion: (newValue, [error]) {
+                if (newValue == null) {
+                  return;
+                }
+                print("get `ready`");
+                expect(newValue.value, equals("ready"));
+              });
+        });
+
+    await record.setPropertyValue("state", PropertyDatabaseValueString("started"));
+    print("`started` passed");
+    await record.setPropertyValue("state", PropertyDatabaseValueString("userActionNeeded"));
+    print("`userActionNeeded` passed");
+    await record.setPropertyValue("state", PropertyDatabaseValueString("ready"));
+    print("`ready` passed");
+  });
+
+  tearDown(() async {
+    appController.databaseController.databasePool.close();
+  });
+}
