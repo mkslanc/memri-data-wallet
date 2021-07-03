75b8ab60c261d5dbcdf04ee664536765373ac904
aniam <amirjanyanani@gmail.com>
2021-07-03 19:18:40 +0400

    print Unexpected CVU Expression types on error

--- lib/MemriApp/CVU/resolving/CVULookupController.dart
+++ lib/MemriApp/CVU/resolving/CVULookupController.dart
@@ -830,7 +830,7 @@ class CVULookupController {
       return await resolve<ItemRecord>(expression: expression.lhs, context: context, db: db) ??
           await resolve<ItemRecord>(expression: expression.rhs, context: context, db: db);
     } else {
-      print("CVU Expression error: Unexpected CVU Expression type");
+      print("CVU Expression error: Unexpected CVU Expression type ${expression.toString()}");
       return null;
     }
   }
@@ -904,7 +904,7 @@ class CVULookupController {
       }
       return [];
     } else {
-      print("CVU Expression error: Unexpected CVU Expression type");
+      print("CVU Expression error: Unexpected CVU Expression type ${expression.toString()}");
       return [];
     }
   }
@@ -947,7 +947,7 @@ class CVULookupController {
         return null;
       }
     } else {
-      print("CVU Expression error: Unexpected CVU Expression type");
+      print("CVU Expression error: Unexpected CVU Expression type ${expression.toString()}");
       return null;
     }
   }
@@ -990,7 +990,7 @@ class CVULookupController {
         return null;
       }
     } else {
-      print("CVU Expression error: Unexpected CVU Expression type");
+      print("CVU Expression error: Unexpected CVU Expression type ${expression.toString()}");
       return null;
     }
   }
@@ -1029,7 +1029,7 @@ class CVULookupController {
           .whereType<String>()
           .join();
     } else {
-      print("CVU Expression error: Unexpected CVU Expression type");
+      print("CVU Expression error: Unexpected CVU Expression type ${expression.toString()}");
       return null;
     }
   }
@@ -1136,7 +1136,7 @@ class CVULookupController {
       }
       return lhs != rhs;
     } else {
-      print("CVU Expression error: Unexpected CVU Expression type");
+      print("CVU Expression error: Unexpected CVU Expression type ${expression.toString()}");
       return null;
     }
   }
