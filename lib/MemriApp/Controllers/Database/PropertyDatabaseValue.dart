88d02a89ee898090ce974794534047220772a65f
aniam <amirjanyanani@gmail.com>
2021-10-11 20:25:29 +0400

    fixed asBool: return true only if 1 or true, return null if not valid

--- lib/MemriApp/Controllers/Database/PropertyDatabaseValue.dart
+++ lib/MemriApp/Controllers/Database/PropertyDatabaseValue.dart
@@ -180,9 +180,12 @@ abstract class PropertyDatabaseValue with EquatableMixin {
   }
 
   bool? asBool() {
-    return (value == null || ["0", "", "false"].contains(value.toString()))
-        ? false
-        : true; //TODO find a valid way to convert to boolean @anijanyan
+    if (["0", "", "false"].contains(value.toString())) {
+      return false;
+    } else if (["1", "true"].contains(value.toString())) {
+      return true;
+    }
+    return null;
   }
 
   DateTime? asDate() {
