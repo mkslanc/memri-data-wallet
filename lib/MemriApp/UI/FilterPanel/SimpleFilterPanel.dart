f8ec5f8a42fa5135ca35de61eabc53cc4c0ae41e
mkslanc <mks-tech@ya.ru>
2021-10-20 17:23:50 +0400

    deleted unnecessary dependency

--- lib/MemriApp/UI/FilterPanel/SimpleFilterPanel.dart
+++ lib/MemriApp/UI/FilterPanel/SimpleFilterPanel.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:memri/MemriApp/UI/Components/Button/ActionButton.dart';
 import 'package:memri/MemriApp/UI/UIHelpers/utilities.dart';
