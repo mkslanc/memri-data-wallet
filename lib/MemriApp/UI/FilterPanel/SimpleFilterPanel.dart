23c863d18a76b5699abc79381c2bbc088fe6bcae
mkslanc <mks-tech@ya.ru>
2021-10-20 16:37:16 +0400

    Filtering by service; displaying service name in Account

--- lib/MemriApp/UI/FilterPanel/SimpleFilterPanel.dart
+++ lib/MemriApp/UI/FilterPanel/SimpleFilterPanel.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:memri/MemriApp/UI/Components/Button/ActionButton.dart';
 import 'package:memri/MemriApp/UI/UIHelpers/utilities.dart';
@@ -18,16 +19,19 @@ class _SimpleFilterPanelState extends State<SimpleFilterPanel> {
   Widget build(BuildContext context) {
     var actions = widget.viewContext.viewDefinitionPropertyResolver.actions("filterButtons");
     return actions != null
-        ? SizedBox(
-            height: 34,
-            child: Row(
-                children: space(
-              10,
-              actions.map((action) => ActionButton(
-                  action: action,
-                  viewContext: widget.viewContext.getCVUContext(),
-                  pageController: widget.viewContext.pageController)),
-            )),
+        ? SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: SizedBox(
+              height: 35,
+              child: Row(
+                  children: space(
+                10,
+                actions.map((action) => ActionButton(
+                    action: action,
+                    viewContext: widget.viewContext.getCVUContext(),
+                    pageController: widget.viewContext.pageController)),
+              )),
+            ),
           )
         : Empty();
   }
