e20a71fea5d017d23008c259d3e8329e29893f97
anijanyan <amirjanyanani@gmail.com>
2021-12-02 16:06:28 +0400

    stop updating for closed pageControllers

--- lib/MemriApp/UI/Chrome/BreadCrumbs.dart
+++ lib/MemriApp/UI/Chrome/BreadCrumbs.dart
@@ -1,7 +1,7 @@
+import 'package:collection/src/iterable_extensions.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:memri/MemriApp/Extensions/BaseTypes/Collection.dart';
 import 'package:memri/MemriApp/UI/CVUComponents/types/CVUFont.dart';
 import 'package:memri/MemriApp/UI/UIHelpers/utilities.dart';
 
@@ -23,24 +23,31 @@ class _BreadCrumbsState extends State<BreadCrumbs> {
   late List<ViewContextHolder> navigationStack;
   late final memri.PageController pageController;
   var titleList = <String>[];
-  late Future<void> _init;
+  late Future<List<String>> _titleList;
 
   @override
   void initState() {
     super.initState();
     pageController = widget.pageController;
     pageController.addListener(updateState);
-    _init = init();
+    _titleList = initTitleList();
   }
 
   updateState() {
-    _init = init();
+    _titleList = initTitleList();
   }
 
-  Future<void> init() async {
+  dispose() {
+    super.dispose();
+    pageController.removeListener(updateState);
+  }
+
+  Future<List<String>> initTitleList() async {
+    if (!pageController.isPageActive) {
+      return [];
+    }
     navigationStack = pageController.navigationStack.state;
-    titleList = [];
-    titleList = await Future.wait(navigationStack.map((ViewContextHolder viewContextHolder) async {
+    return await Future.wait(navigationStack.map((ViewContextHolder viewContextHolder) async {
       var viewContextController = pageController.makeContext(viewContextHolder);
       return (await viewContextController.viewDefinitionPropertyResolver.string("title") ??
               (viewContextController.focusedItem != null
@@ -51,40 +58,39 @@ class _BreadCrumbsState extends State<BreadCrumbs> {
   }
 
   @override
-  void dispose() {
-    super.dispose();
-    pageController.removeListener(updateState);
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: _init,
-      builder: (context, snapshot) => titleList.length > 1
-          ? Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: titleList
-                  .mapIndexed((index, title) {
-                    var isLast = index == titleList.length - 1;
-                    return TextButton(
-                      onPressed: () => isLast ? null : pageController.navigateTo(index),
-                      child: Text(
-                        title,
-                        style: isLast
-                            ? CVUFont.tabList.copyWith(color: Color(0xFF999999))
-                            : CVUFont.tabList,
-                      ),
-                    );
-                  })
-                  .expand((item) => [
-                        SvgPicture.asset(
-                          "assets/images/brcmb_line.svg",
+    return FutureBuilder<List<String>>(
+      future: _titleList,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          titleList = snapshot.data!;
+        }
+        return titleList.length > 1
+            ? Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: titleList
+                    .mapIndexed((index, title) {
+                      var isLast = index == titleList.length - 1;
+                      return TextButton(
+                        onPressed: () => isLast ? null : pageController.navigateTo(index),
+                        child: Text(
+                          title,
+                          style: isLast
+                              ? CVUFont.tabList.copyWith(color: Color(0xFF999999))
+                              : CVUFont.tabList,
                         ),
-                        item
-                      ])
-                  .skip(1)
-                  .toList())
-          : Empty(),
+                      );
+                    })
+                    .expand((item) => [
+                          SvgPicture.asset(
+                            "assets/images/brcmb_line.svg",
+                          ),
+                          item
+                        ])
+                    .skip(1)
+                    .toList())
+            : Empty();
+      },
     );
   }
 }
