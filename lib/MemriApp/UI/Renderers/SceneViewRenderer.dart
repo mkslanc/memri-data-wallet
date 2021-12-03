509b233c7dc793d4c81f5a12b41deb7988dd1a46
Azat Alimov <mks-tech@ya.ru>
2021-12-03 09:47:20 +0000

    unnecessary cupertino imports removed

--- lib/MemriApp/UI/Renderers/SceneViewRenderer.dart
+++ lib/MemriApp/UI/Renderers/SceneViewRenderer.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:memri/MemriApp/CVU/definitions/CVUValue.dart';
