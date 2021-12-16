63f286b0a91510add2746cabed26964c489f967c
mkslanc <mks-tech@ya.ru>
2021-12-16 19:08:05 +0400

    added `tab` action for cvu Editor

--- lib/MemriApp/UI/Renderers/CVUEditorRenderer.dart
+++ lib/MemriApp/UI/Renderers/CVUEditorRenderer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:memri/MemriApp/CVU/CVUController.dart';
 import 'package:memri/MemriApp/CVU/definitions/CVUParsedDefinition.dart';
@@ -182,16 +183,25 @@ class _CVUEditorRendererViewState extends State<CVUEditorRendererView> {
             future: _init,
             builder: (context, snapshot) => Padding(
                 padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 30),
-                child: TextFormField(
-                  decoration: InputDecoration(),
-                  style: TextStyle(color: CVUColor.white),
-                  controller: controller,
-                  onChanged: (String newValue) async {
-                    newCVU = newValue;
-                  },
-                  keyboardType: TextInputType.multiline,
-                  minLines: 6,
-                  maxLines: null,
+                child: Actions(
+                  actions: {InsertTabIntent: InsertTabAction()},
+                  child: Shortcuts(
+                    shortcuts: {
+                      LogicalKeySet(LogicalKeyboardKey.tab): InsertTabIntent(4, controller)
+                    },
+                    child: TextFormField(
+                      decoration: InputDecoration(),
+                      style: TextStyle(color: CVUColor.white),
+                      controller: controller,
+                      onChanged: (String newValue) async {
+                        newCVU = newValue;
+                      },
+                      textInputAction: TextInputAction.newline,
+                      keyboardType: TextInputType.multiline,
+                      minLines: 6,
+                      maxLines: null,
+                    ),
+                  ),
                 )),
           )),
         ],
@@ -221,5 +231,37 @@ class _CVUEditorRendererViewState extends State<CVUEditorRendererView> {
   close() {
     widget.pageController.sceneController.pageControllers.first.topMostContext?.config.cols = null;
     widget.pageController.sceneController.removePageController(widget.pageController);
+  }
+}
+
+class InsertTabIntent extends Intent {
+  const InsertTabIntent(this.numSpaces, this.textController);
+  final int numSpaces;
+  final TextEditingController textController;
+}
+
+class InsertTabAction extends Action {
+  @override
+  Object invoke(covariant Intent intent) {
+    if (intent is InsertTabIntent) {
+      final oldValue = intent.textController.value;
+      final newComposing = TextRange.collapsed(oldValue.composing.start);
+      final newSelection =
+          TextSelection.collapsed(offset: oldValue.selection.start + intent.numSpaces);
+
+      final newText = StringBuffer(oldValue.selection.isValid
+          ? oldValue.selection.textBefore(oldValue.text)
+          : oldValue.text);
+      for (var i = 0; i < intent.numSpaces; i++) {
+        newText.write(' ');
+      }
+      newText.write(oldValue.selection.isValid ? oldValue.selection.textAfter(oldValue.text) : '');
+      intent.textController.value = intent.textController.value.copyWith(
+        composing: newComposing,
+        text: newText.toString(),
+        selection: newSelection,
+      );
+    }
+    return '';
   }
 }
