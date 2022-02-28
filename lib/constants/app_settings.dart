708aa619c8d121e86ad08570c126930b6349e550
Erfan <erfan.jazebnikoo@gmail.com>
2022-02-28 13:04:02 +0300

    CU-2703c9x_Implement-logger_Erfan-Jazeb-Nikoo
     - Added the logger plugin
     - Implemented the app_logger
     - Updated the app_settings

--- lib/constants/app_settings.dart
+++ lib/constants/app_settings.dart
@@ -1,8 +1,16 @@
+import 'package:logger/logger.dart';
+
 class AppSettings {
   AppSettings._();
 
   static const String defaultPodURL = "https://dev.pod.memri.io";
   static const String defaultDevPodURL = "http://localhost:3030";
 
+  /// Database
   static const String defaultDatabase = "dev_database";
+
+  /// Logger
+  static const Level loggerLevel = Level.debug;
+  static const bool loggerColorEnabled = true;
+  static const bool loggerPrintTime = true;
 }
