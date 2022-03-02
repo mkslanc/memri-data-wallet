16b2a5ff5da87487debe54ad7dcfc272e37a55a3
Erfan <erfan.jazebnikoo@gmail.com>
2022-03-02 16:13:55 +0300

    branch CU-26960f1_database-enhancement
     - Fixed the merge conflicts

--- lib/constants/app_settings.dart
+++ lib/constants/app_settings.dart
@@ -8,7 +8,7 @@ class AppSettings {
 
   /// Database
   static const String defaultDatabase = "dev_database";
-  static const int syncControllerInterval = 3;
+  static const int syncControllerIntervalSecs = 3;
 
   /// Logger
   static const Level loggerLevel = Level.debug;
