b58f0e20958e64b8c46fe3f1b1f6c970fc5caf64
anijanyan <amirjanyanani@gmail.com>
2022-03-13 23:48:56 +0400

    onSurface (disabled) style

--- lib/constants/app_styles.dart
+++ lib/constants/app_styles.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:memri/constants/cvu/cvu_font.dart';
 
 final ButtonStyle primaryButtonStyle = TextButton.styleFrom(
-  backgroundColor: Color(0xff333333),
-  primary: Color(0xffF5F5F5),
-  textStyle: CVUFont.buttonLabel,
-  minimumSize: Size.zero,
-  tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-  padding: const EdgeInsets.all(10),
-);
+    backgroundColor: Color(0xff333333),
+    primary: Color(0xffF5F5F5),
+    textStyle: CVUFont.buttonLabel,
+    minimumSize: Size.zero,
+    tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+    padding: const EdgeInsets.all(10),
+    onSurface: Colors.white);
 
 final Map<String, ButtonStyle> buttonStyles = {"primaryButton": primaryButtonStyle};
