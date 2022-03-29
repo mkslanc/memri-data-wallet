1864717a83f667bdf11a26b41672ff13864270d2
Erfan <erfan.jazebnikoo@gmail.com>
2022-03-29 12:29:30 +0300

    CU-276g5tu_600_projects---where-am-I-feedback-missing-projects-not-highlighted-project-name-not-visible
     Changes:
    
      - Added the current view name in the main_navigation_view
      - Updated main_navigation_view with
      - Updated the callback behavior in the navigation_pane_view

--- lib/widgets/navigation/main_navigation_view.dart
+++ lib/widgets/navigation/main_navigation_view.dart
@@ -6,14 +6,23 @@ import 'navigation_pane_view.dart';
 
 class MainNavigationView extends StatefulWidget {
   final SceneController sceneController;
+  final String? currentViewName;
 
-  MainNavigationView({required this.sceneController});
+  MainNavigationView({required this.sceneController, this.currentViewName});
 
   @override
   _MainNavigationViewState createState() => _MainNavigationViewState();
 }
 
 class _MainNavigationViewState extends State<MainNavigationView> {
+  String? _currentViewName;
+
+  @override
+  void initState() {
+    _currentViewName = widget.currentViewName;
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -24,39 +33,34 @@ class _MainNavigationViewState extends State<MainNavigationView> {
         SizedBox(
           width: 34,
         ),
-        /*SvgPicture.asset("assets/images/ico_search.svg"),*/ //TODO: uncomment this after search implemented
+        /*SvgPicture.asset("assets/images/ico_search.svg"),*/
+        //TODO: uncomment this after search implemented
         Spacer(),
-        NavigationItemView(
-          item: Item(name: 'Workspace', targetViewName: "home"),
-          sceneController: widget.sceneController,
-        ),
-        SizedBox(
-          width: 30,
-        ),
-        NavigationItemView(
-          item: Item(name: 'Data', targetViewName: "allData"),
-          sceneController: widget.sceneController,
-        ),
-        SizedBox(
-          width: 30,
-        ),
-        NavigationItemView(
-          item: Item(name: 'Projects', targetViewName: "allProjects"),
-          sceneController: widget.sceneController,
-        ),
-        SizedBox(
-          width: 30,
-        ),
-        NavigationItemView(
-          item: Item(name: 'Apps', targetViewName: "apps-and-plugins"),
-          sceneController: widget.sceneController,
-        ),
+        _buildNavigationItemView('Workspace', 'home'),
+        SizedBox(width: 30),
+        _buildNavigationItemView('Data', 'allData'),
+        SizedBox(width: 30),
+        _buildNavigationItemView('Projects', 'allProjects'),
+        SizedBox(width: 30),
+        _buildNavigationItemView('Apps', 'apps-and-plugins'),
         Spacer(),
         InkWell(
             onTap: () => widget.sceneController
                 .navigateToNewContext(clearStack: true, animated: false, viewName: "home"),
             child: SvgPicture.asset("assets/images/logo.svg"))
       ],
+    );
+  }
+
+  Widget _buildNavigationItemView(String name, String viewName) {
+    return NavigationItemView(
+      item: Item(
+        name: name,
+        targetViewName: viewName,
+        callback: () => setState(() => _currentViewName = viewName),
+      ),
+      textColor: _currentViewName == viewName ? Color(0xffFE570F) : Colors.black,
+      sceneController: widget.sceneController,
     );
   }
 }
