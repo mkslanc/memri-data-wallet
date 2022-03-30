23ea43fbfa3d67fe1a0ce51d8cb9a31357d07eba
anijanyan <amirjanyanani@gmail.com>
2022-03-30 20:21:46 +0400

    fixed viewArguments fromJson/toJson parentArguments

--- lib/models/cvu/cvu_view_arguments.dart
+++ lib/models/cvu/cvu_view_arguments.dart
@@ -22,13 +22,14 @@ class CVUViewArguments with EquatableMixin {
   List<ItemRecord>? argumentItems;
 
   // The view arguments of the parent view (used to resolve)
-  @JsonKey(ignore: true)
   CVUViewArguments? parentArguments;
 
   // The view arguments of the sub views (used to resolve)
+  @JsonKey(ignore: true)
   Map<String, CVUViewArguments> subViewArguments = {};
 
   //used to deserialize from json for parentArguments //TODO maybe there's better way
+  @JsonKey(ignore: true)
   List<CVUViewArguments> childrenArguments = [];
 
   CVUViewArguments(
@@ -37,13 +38,7 @@ class CVUViewArguments with EquatableMixin {
     this.parentArguments?.childrenArguments.add(this);
   }
 
-  factory CVUViewArguments.fromJson(Map<String, dynamic> json) {
-    var viewArguments = _$CVUViewArgumentsFromJson(json);
-    viewArguments.childrenArguments.forEach((subArgument) {
-      subArgument.parentArguments = viewArguments;
-    });
-    return viewArguments;
-  }
+  factory CVUViewArguments.fromJson(Map<String, dynamic> json) => _$CVUViewArgumentsFromJson(json);
 
   Map<String, dynamic> toJson() => _$CVUViewArgumentsToJson(this);
 
