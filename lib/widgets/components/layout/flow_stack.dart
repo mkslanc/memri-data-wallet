6fb763006cd31d268afbee2304d25b1f8b5fb3ca
anijanyan <amirjanyanani@gmail.com>
2022-05-27 22:12:43 +0400

    flowstack direction parametric

--- lib/widgets/components/layout/flow_stack.dart
+++ lib/widgets/components/layout/flow_stack.dart
@@ -6,6 +6,7 @@ class FlowStack<T> extends StatelessWidget {
   final List<T> data;
   final Point spacing;
   final WrapCrossAlignment alignment;
+  final Axis direction;
 
   final List<Widget> Function(T) content;
 
@@ -13,6 +14,7 @@ class FlowStack<T> extends StatelessWidget {
       {required this.data,
       spacing,
       this.alignment = WrapCrossAlignment.start,
+      this.direction = Axis.horizontal,
       required this.content})
       : this.spacing = spacing ?? Point(0, 0);
 
@@ -23,6 +25,7 @@ class FlowStack<T> extends StatelessWidget {
         children: [
           ColoredBox(color: Colors.transparent),
           Wrap(
+            direction: direction,
             alignment: WrapAlignment.center,
             crossAxisAlignment: alignment,
             spacing: spacing.y.toDouble(),
