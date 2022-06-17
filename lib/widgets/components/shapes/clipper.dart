d6825cb5114121e6f49b6252e9605ac6c1fe0ba5
anijanyan <amirjanyanani@gmail.com>
2022-06-17 18:54:55 +0400

    TriangleClipper

---
+++ lib/widgets/components/shapes/clipper.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/rendering.dart';
+
+class TriangleClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    final path = Path();
+    path.moveTo(size.width / 2, 0);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(TriangleClipper oldClipper) => false;
+}
