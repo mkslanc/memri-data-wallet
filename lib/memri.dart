62ca8cb56aef037c09f35f514fe1dc39a93e67a0
Erfan Jazeb Nikoo <erfan.jazebnikoo@gmail.com>
2022-06-21 13:01:10 +0300

    PROD_SC-828_Flutter-Switcheroo--ARCHT---Localization_Erfan-Jazeb-Nikoo

--- lib/memri.dart
+++ lib/memri.dart
@@ -1,8 +1,11 @@
 import 'package:fluro/fluro.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:memri/configs/routes/route_navigator.dart';
 import 'package:memri/constants/app_themes.dart';
+import 'package:memri/localization/generated/l10n.dart';
 import 'package:memri/screens/not_found_screen.dart';
+import 'package:memri/utils/app_helper.dart';
 
 class Memri extends StatefulWidget {
   const Memri({Key? key}) : super(key: key);
@@ -12,6 +15,9 @@ class Memri extends StatefulWidget {
 }
 
 class _MemriState extends State<Memri> {
+  final List<Locale> _deviceLocales = WidgetsBinding.instance.window.locales;
+  Locale _locale = app.locales.enUS;
+
   @override
   void initState() {
     final router = FluroRouter();
@@ -27,6 +33,42 @@ class _MemriState extends State<Memri> {
       theme: lightTheme,
       onGenerateRoute: RouteNavigator.router.generator,
       onUnknownRoute: (_) => MaterialPageRoute(builder: (_) => NotFoundScreen()),
+      locale: _locale,
+      localizationsDelegates: const [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate, //provides localised strings
+        GlobalWidgetsLocalizations.delegate, //provides RTL support
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      localeListResolutionCallback: (_, supportedLocales) {
+        if (app.locales.useCustomLocale) {
+          return app.locales.appLocale;
+        }
+
+        Locale? locale;
+        if (_deviceLocales.isNotEmpty) {
+          var intersection = _deviceLocales
+              .where((deviceLocale) => supportedLocales
+              .map((supportedLocale) => supportedLocale.languageCode)
+              .toList()
+              .contains(deviceLocale.languageCode))
+              .toList();
+          if (intersection.isNotEmpty) {
+            locale = intersection.first;
+          }
+        }
+        locale ??= supportedLocales.first;
+
+        if (app.locales.systemAppLocale?.languageCode !=
+            locale.languageCode) {
+          WidgetsBinding.instance.addPostFrameCallback(
+                  (_) => app.locales.systemAppLocale = locale!);
+        }
+
+        return locale;
+      },
+      // Tells the system which are the supported languages
+      supportedLocales: S.delegate.supportedLocales,
     );
   }
 }
