ee223d06b3fc53879e630c4ddf0d18c364b3e5d0
Koen van der Veen <koenlennartvanderveen@gmail.com>
2022-06-21 10:18:31 +0200

    inbox init

--- lib/screens/workspace/apps/apps_inbox_screen.dart
+++ lib/screens/workspace/apps/apps_inbox_screen.dart
@@ -1,6 +1,14 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:memri/controllers/app_controller.dart';
+import 'package:memri/models/database/item_record.dart';
 import 'package:memri/widgets/navigation/navigation_appbar.dart';
 import 'package:memri/widgets/scaffold/workspace_scaffold.dart';
+
+import '../../../controllers/database_query.dart';
+import '../../../controllers/file_storage/web_file_storage_controller.dart';
+import '../../../core/services/database/property_database_value.dart';
 
 class AppsInboxScreen extends StatefulWidget {
   final showMainNavigation;
@@ -20,8 +28,177 @@ class _AppsInboxScreenState extends State<AppsInboxScreen> {
       child: Container(
         width: MediaQuery.of(context).size.width,
         padding: EdgeInsets.fromLTRB(30, 30, 0, 30),
-        child: Container(child: Text(widget.importer ?? "")),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              flex: 1,
+              child: ListView.builder(
+                  itemCount: chats.length,
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) {
+                    return InkWell(
+                        onTap: () {
+                          setActiveChat();
+                        },
+                        child: Container(
+                           padding: new EdgeInsets.symmetric(
+                                                  vertical: 5, horizontal: 0),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: ListTile(
+                                  leading: CircleAvatar(
+                                      radius: 16.0,
+                                      backgroundImage:
+                                          // chats_images.elementAt(index) == null ? Image.asset('assets/images/person.png'): Image(image: chats_images[index]!),
+                                          chats_images.length > index && chats_images[index] != null
+                                              ? chats_images[index]!
+                                              : AssetImage(
+                                                  'assets/images/person.png'),
+                                      backgroundColor: Colors.transparent),
+                                  title: FutureBuilder(
+                                      future: chats[index].propertyValue('name'),
+                                      builder: (context, value) {
+                                        if (!value.hasData) return Text("");
+                                        return Text(
+                                            (value.data! as PropertyDatabaseValue)
+                                                .value
+                                                .toString(),
+                                            style: TextStyle(fontSize: 13));
+                                      }),
+                                ),
+                              ),
+                              IconButton(
+                                  onPressed: () {},
+                                  icon: Icon(Icons.more_horiz,
+                                      color: Color(0xFFDFDEDE)))
+                            ],
+                          ),
+                        ));
+                  }),
+            ),
+            Expanded(
+                flex: 2,
+                child: ListView.builder(
+                    itemCount: activeChatMessages.length,
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                      return Row(
+                        children: [
+                          Expanded(
+                            child: ListTile(
+                              leading: CircleAvatar(
+                                  child:
+                                      Image.asset('assets/images/person.png'),
+                                  backgroundColor: Colors.transparent),
+                              title: FutureBuilder(
+                                  future: activeChatMessages[index]
+                                      .propertyValue('content'),
+                                  builder: (context, value) {
+                                    if (!value.hasData)
+                                      return Text("");
+                                    else {
+                                      var rng = Random();
+                                      var by_me = rng.nextInt(2) == 0;
+                                      return Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Container(
+                                              padding: new EdgeInsets.symmetric(
+                                                  vertical: 10, horizontal: 15),
+                                              child: Text(
+                                                (value.data!
+                                                        as PropertyDatabaseValue)
+                                                    .value
+                                                    .toString(),
+                                                style: TextStyle(
+                                                    fontSize: 13,
+                                                    color: by_me
+                                                        ? Color(0xFFFBFBFB)
+                                                        : Color(0xFF333333)),
+                                              ),
+                                              decoration: BoxDecoration(
+                                                color: by_me
+                                                    ? Color(0xFF4F56FE)
+                                                    : Color(0xFFF8F8F8),
+                                                borderRadius: BorderRadius.only(
+                                                    topRight:
+                                                        Radius.circular(10),
+                                                    bottomRight:
+                                                        Radius.circular(10),
+                                                    bottomLeft:
+                                                        Radius.circular(10)),
+                                              )),
+                                        ],
+                                      );
+                                    }
+                                  }),
+                            ),
+                          )
+                        ],
+                      );
+                    })),
+          ],
+        ),
       ),
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getChats();
+  }
+
+  List<ItemRecord> chats = [];
+  List<ItemRecord> activeChatMessages = [];
+  List<ImageProvider?> chats_images = [];
+  late Stream result;
+
+  void getChats() async {
+    var queryDef = DatabaseQueryConfig(
+        itemTypes: ["MessageChannel"], pageSize: 1000, currentPage: 0);
+    result = queryDef.executeRequest(AppController.shared.databaseController);
+
+    print("BBB");
+    result.asBroadcastStream().listen((event) async {
+      List<ImageProvider?> _chats_images = [];
+      for (ItemRecord c in event as List<ItemRecord>) {
+        var photoItem = await c.edgeItem("photo");
+        var fileItem = await photoItem?.edgeItem("file");
+        
+        var fileID = (await fileItem?.propertyValue("sha256"))?.asString();
+
+        if (fileID == null) {
+          chats_images.add(null);
+        } else {
+          print("CCCC");
+          print(fileID);
+          String fileURL = FileStorageController.getURLForFile(fileID);
+          var imageProvider = await FileStorageController.getImage(fileURL: fileURL);
+          _chats_images.add(imageProvider);
+        }
+      }
+      setState(() {
+        chats = event as List<ItemRecord>;
+        chats_images = _chats_images;
+        //  image: {{.sender.owner.profilePicture OR .sender.profilePicture OR "assets/images/person.png"}}
+      });
+    });
+  }
+
+  void setActiveChat() async {
+    var queryDef = DatabaseQueryConfig(
+        itemTypes: ["Message"], pageSize: 30, currentPage: 0);
+    result = queryDef.executeRequest(AppController.shared.databaseController);
+
+    print(result);
+    result.asBroadcastStream().listen((event) {
+      setState(() {
+        activeChatMessages = event as List<ItemRecord>;
+      });
+    });
   }
 }
