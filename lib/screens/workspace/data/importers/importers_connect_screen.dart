a7bcd0debbf1ff1e4f4dac161c5a6083ef3f4649
Koen van der Veen <koenlennartvanderveen@gmail.com>
2022-06-22 09:20:49 +0200

    authentication screen

--- lib/screens/workspace/data/importers/importers_connect_screen.dart
+++ lib/screens/workspace/data/importers/importers_connect_screen.dart
@@ -2,10 +2,6 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:memri/configs/routes/route_navigator.dart';
-import 'package:memri/core/apis/pod/pod_payloads.dart';
-import 'package:memri/core/controllers/app_controller.dart';
-import 'package:memri/utilities/helpers/app_helper.dart';
 import 'package:memri/widgets/navigation/navigation_appbar.dart';
 import 'package:memri/widgets/scaffold/workspace_scaffold.dart';
 import 'package:uuid/uuid.dart';
@@ -23,6 +19,20 @@ class ImporterInfoLine extends StatelessWidget {
   final List<FontWeight> weights;
   final List<String> contents;
 
+import '../../../../constants/app_logger.dart';
+import '../../../../controllers/app_controller.dart';
+import '../../../../core/services/database/property_database_value.dart';
+import '../../../../core/services/mixpanel_analytics_service.dart';
+import '../../../../core/services/plugin_handler.dart';
+import '../../../../models/database/item_property_record.dart';
+import '../../../../models/database/item_record.dart';
+import '../../../../widgets/components/html_view/html_view.dart';
+
+class ImporterInfoLine extends StatelessWidget {
+  const ImporterInfoLine({Key? key, required this.content}) : super(key: key);
+
+  final String content;
+
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -30,15 +40,13 @@ class ImporterInfoLine extends StatelessWidget {
         Container(
             padding: new EdgeInsets.only(top: 12, bottom: 12, right: 12),
             child: SvgPicture.asset("assets/images/arrow-right-circle.svg")),
-        for (int i = 0; i < contents.length; i++)
-          Text(contents[i],
-              style: TextStyle(fontSize: 14, fontWeight: weights[i]))
+        Text(content, style: TextStyle(fontSize: 14)),
       ],
     );
   }
 }
 
-class ImportersConnectScreen extends StatefulWidget {
+class ImportersConnectScreen extends StatelessWidget {
   const ImportersConnectScreen({Key? key}) : super(key: key);
 
   @override
@@ -56,88 +64,27 @@ class _ImportersConnectScreenState extends State<ImportersConnectScreen> {
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              // Row(
-              //   children: [
-              //     Text("Step 2", style: TextStyle(fontSize: 12)),
-              //   ],
-              // ),
+              Row(
+                children: [
+                  Text("Step 2", style: TextStyle(fontSize: 12)),
+                ],
+              ),
               Row(
                 children: [
                   Text("On your phone", style: TextStyle(fontSize: 30)),
                 ],
               ),
+              ImporterInfoLine(content: "Open Whatsapp"),
               ImporterInfoLine(
-                  contents: ["Open", " Whatsapp"],
-                  weights: [FontWeight.normal, FontWeight.bold]),
+                  content: "Tap settings and select linked devices"),
+              ImporterInfoLine(content: "Tap link device"),
               ImporterInfoLine(
-                contents: [
-                  "Tap",
-                  " settings",
-                  " and select",
-                  " Linked devices"
-                ],
-                weights: [
-                  FontWeight.normal,
-                  FontWeight.bold,
-                  FontWeight.normal,
-                  FontWeight.bold
-                ],
-              ),
-              ImporterInfoLine(
-                contents: ["Tap", " Link a device"],
-                weights: [FontWeight.normal, FontWeight.bold],
-              ),
-              ImporterInfoLine(
-                contents: [
-                  "Point your phone to the QR code on the screen of your computer to",
-                  " capture the code"
-                ],
-                weights: [FontWeight.normal, FontWeight.bold],
-              ),
+                  content:
+                      "Point your phone to the QR code on the screen of your computer and capture the code"),
               Container(
-                  child: (() {
-                if ((status == "idle" && url == null) || status == "started") {
-                  return Container(
-                      height: 350,
-                      constraints: BoxConstraints(maxWidth: 350),
-                      // color:Color.fromARGB(194, 66, 14, 14),
-                      color: Color(0xffF6F6F6),
-                      padding: EdgeInsets.all(32),
-                      child: Center(
-                          child: Text(
-                              "Please wait for your QR code to be generated. This make take a few moments.",
-                              style: TextStyle(
-                                  fontSize: 14,
-                                  color: app.colors.brandGreyText))));
-                } else if (status == "userActionNeeded") {
-                  return Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                          // dont change this height, it will cut off the qr code
-                          height: 350,
-                          width: 350,
-                          child: HtmlView(src: url, reload: true)),
-                    ],
-                  );
-                } else if (status == "daemon") {
-                  return Container(
-                      child: Row(
-                    children: [
-                      Text(
-                        "Signing in",
-                        style: TextStyle(
-                            color: app.colors.brandOrange, fontSize: 14),
-                      ),
-                    ],
-                  ));
-                } else if (status == "error") {
-                  return Text("Something went wrong");
-                }
-              }())
-                  // url==null ? CircularProgressIndicator(): HtmlView(src: url, reload: true)),
-                  )
+                  height: 300,
+                  width: 300,
+                  child: HtmlView(src: "https://docs.memri.io", reload: true)),
             ],
           )),
     );
@@ -229,6 +176,71 @@ class _ImportersConnectScreenState extends State<ImportersConnectScreen> {
               }
             });
           }));
+    } catch (error) {
+      AppLogger.err("Error starting plugin: $error");
+    }
+  }
+
+  void execute() async {
+    // var lookup = CVULookupController();
+    // var db = pageController.appController.databaseController;
+    var db = AppController.shared.databaseController;
+
+    // TODO, what are we doing with things from the default database?
+    var pluginIdValue = 20004000000;
+    var pluginModuleValue = "whatsapp.plugin";
+    var pluginNameValue = "WhatsappPlugin"
+    var containerValue = "gitlab.memri.io:5050/memri/plugins/whatsapp-multi-device:dev-latest";
+    // if (pluginIdValue == null ||
+    //     containerValue == null ||
+    //     pluginModuleValue == null ||
+    //     pluginNameValue == null) {
+    //   AppLogger.warn("Not all params provided for PluginRun");
+    //   return;
+    // }
+    // var configValue = vars["config"];
+
+    // String? pluginId = await lookup.resolve<String>(value: pluginIdValue, context: context, db: db);
+
+    // String? container =
+    //     await lookup.resolve<String>(value: containerValue, context: context, db: db);
+    // if (container == null) return;
+
+    // TODO, make more robust
+    ItemRecord plugin = (await ItemRecord.fetchWithUID("20004", db))!;
+
+    // String? pluginModule =
+    //     await lookup.resolve<String>(value: pluginModuleValue, context: context, db: db) ?? "";
+    // String? pluginName =
+    //     await lookup.resolve<String>(value: pluginNameValue, context: context, db: db) ?? "";
+    // String? config;
+    // if (configValue != null) {
+    //   config = await lookup.resolve<String>(value: configValue, context: context, db: db) ?? "";
+    // }
+    try {
+      var pluginRunItem = ItemRecord(type: "PluginRun");
+      var propertyRecords = [
+        ItemPropertyRecord(
+            name: "targetItemId", value: PropertyDatabaseValueString(pluginRunItem.uid)),
+        ItemPropertyRecord(name: "pluginModule", value: PropertyDatabaseValueString(pluginModuleValue)),
+        ItemPropertyRecord(name: "pluginName", value: PropertyDatabaseValueString(pluginNameValue)),
+        ItemPropertyRecord(name: "containerImage", value: PropertyDatabaseValueString(containerValue)),
+        ItemPropertyRecord(name: "status", value: PropertyDatabaseValueString("idle")),
+      ];
+
+      // if (config != null) {
+      //   propertyRecords
+      //       .add(ItemPropertyRecord(name: "config", value: PropertyDatabaseValueString(config)));
+      // }
+
+      await pluginRunItem.save();
+      await pluginRunItem.addEdge(edgeName: "plugin", targetItem: plugin);
+      await pluginRunItem.setPropertyValueList(propertyRecords, db: db);
+
+      MixpanelAnalyticsService().logImporterConnect(pluginNameValue);
+      // TODO: fix handling updated plugin
+      await PluginHandler.run(
+          plugin: plugin, runner: pluginRunItem, context: context);
     } catch (error) {
       AppLogger.err("Error starting plugin: $error");
     }
