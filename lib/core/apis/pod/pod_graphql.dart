9306e1f32e914d21d53b2b02484c5d691a117b45
Eelco van der Wel <eelcovdw@gmail.com>
2022-06-24 11:22:53 +0200

    queries for inbox

--- lib/core/apis/pod/pod_graphql.dart
+++ lib/core/apis/pod/pod_graphql.dart
@@ -1,16 +1,176 @@
 import 'dart:convert';
-import 'package:memri/controllers/pod_api.dart';
-import 'package:memri/core/apis/pod/item.dart';
+
+import '../../../constants/app_logger.dart';
+import "pod_requests.dart";
 import "pod_connection_details.dart";
 import 'package:http/http.dart' as http;
+import 'package:collection/collection.dart';
 
+class Item {
+  String type;
+  Map<String, dynamic> properties;
+  Map<String, EdgeList> edges;
 
+  Item({
+    required String this.type,
+    Map<String, dynamic>? properties,
+    Map<String, EdgeList>? edges,
+  })  : properties = properties ?? {},
+        edges = edges ?? {};
 
+  EdgeList? getEdges(String edgeName) {
+    return this.edges[edgeName] ?? null;
+  }
 
+  Item? edgeItem(String edgeName) {
+    // Returns first edge target, if it exists.
+    return this.getEdges(edgeName)?.first();
+  }
+
+  dynamic get(String propertyName) {
+    return this.properties[propertyName] ?? null;
+  }
+
+  dynamic propertyValue(String propertyName) {
+    return this.get(propertyName);
+  }
+
+  static Item fromJson(Map<String, dynamic> itemMap) {
+    String type = "Item";
+    Map<String, dynamic> properties = {};
+    Map<String, EdgeList> edges = {};
+    itemMap.forEach((key, value) {
+      if (key == "type") {
+        type = value;
+      } else if (value is List) {
+        List<Item> targets = [];
+        value.forEach((edgeMap) {
+          targets.add(Item.fromJson(edgeMap));
+        });
+        edges[key] = EdgeList(name: key, targets: targets);
+      } else {
+        properties[key] = value;
+      }
+    });
+    return Item(
+      type: type,
+      properties: properties,
+      edges: edges,
+    );
+  }
+}
+
+class EdgeList {
+  // EdgeList is a separate list to support future edge functionalities
+  String name;
+  List<Item> targets;
+
+  EdgeList({
+    required this.name,
+    List<Item>? targets,
+  }) : targets = targets ?? [];
+
+  Item? first() {
+    return this.targets.firstOrNull;
+  }
+}
+
+Future<http.Response?> execute_graphql(
+    PodConnectionDetails connection, String query) async {
+  var request = PodStandardRequest.queryGraphQL(query);
+  var response = await request.execute(connection);
+  if (response.statusCode != 200) {
+    var error_msg = "ERROR: ${response.statusCode} ${response.reasonPhrase}";
+    AppLogger.err(error_msg);
+    return null;
+  }
+  return response;
+}
+
+List<Item> parseGQLResponse(http.Response? response) {
+  // TODO error handling. Returns an empty list on bad response for now.
+  if (response == null) {
+    return [];
+  }
+  List<Item> result = [];
+  Map<String, dynamic> body = jsonDecode(response.body);
+  List<dynamic> data = body['data'] ?? [];
+
+  data.forEach((itemMap) => result.add(Item.fromJson(itemMap)));
+
+  return result;
+}
 
 // Dataset
 
+Future<List<Item>> getDataset(
+    PodConnectionDetails connection, String datasetName) async {
+  var query = '''
+    query {
+      Dataset (filter: {name: {eq: "$datasetName"}}) {
+          type
+          id
+          name
+          entry (limit: 5000, offset: 0) {
+              id
+              data {
+                  content
+              }
+              annotation {
+                  labelValue
+              }
+          }
+      }
+  }''';
+  var response = await execute_graphql(connection, query);
+  return parseGQLResponse(response);
+}
 
+// Inbox
+
+Future<List<Item>> getMessageChannels(PodConnectionDetails connection) async {
+  var query = '''
+    query {
+      MessageChannel (limit: 1000) {
+        id
+        name
+        photo {
+          id
+          file {
+            sha256
+          }
+        }
+        ~messageChannel (limit: 1, order_desc: dateReceived) {
+          dateReceived
+        }
+      }
+    }
+  ''';
+
+  var response = await execute_graphql(connection, query);
+  return parseGQLResponse(response);
+}
+
+Future<List<Item>> getChannelMessages(
+    {required PodConnectionDetails connection,
+    required String chatID,
+    int limit = 100,
+    int offset = 0}) async {
+  var query = '''
+    query {
+      MessageChannel (filter: {id: {eq: "$chatID"}}) {
+        id
+        ~messageChannel (limit: $limit, offset: $offset) {
+          dateReceived
+          content
+        }
+      }
+    }
+  ''';
+
+  var response = await execute_graphql(connection, query);
+  return parseGQLResponse(response);
+}
 
 // Example usage
 // TODO remove
