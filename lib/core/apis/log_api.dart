d92811bcce8d4309bd8d64d674421baa165f120a
Erfan Jazeb Nikoo <erfan.jazebnikoo@gmail.com>
2022-06-27 10:02:55 +0300

    PROD_SC-388_Flutter-Switcheroo--ARCHT---Implement-logic-behind-UI-in-specific-providers

--- lib/core/apis/log_api.dart
+++ lib/core/apis/log_api.dart
@@ -4,9 +4,9 @@ class LogAPI extends BaseAPI {
   LogAPI() : super('/logs');
 
   Future<String?> saveAll(List<String> logs) async {
-    // var endpoint = '$baseUrl/save-all';
-    // var response = await dio.post(endpoint, data: logs,options: Options());
-    // checkResponseError(response);
-    // return response.data;
+    var endpoint = '$baseUrl/save-all';
+    var response = await dio.post(endpoint, data: logs);
+    checkResponseError(response);
+    return response.data;
   }
 }
