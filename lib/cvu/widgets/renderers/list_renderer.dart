049cb676685e79381efe26a83d8d93003a8c059f
anijanyan <amirjanyanani@gmail.com>
2022-07-06 16:15:44 +0400

    fix for listrenderer

--- lib/cvu/widgets/renderers/list_renderer.dart
+++ lib/cvu/widgets/renderers/list_renderer.dart
@@ -3,7 +3,6 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:memri/core/models/item.dart';
 import 'package:memri/cvu/constants/cvu_color.dart';
-import 'package:memri/utilities/extensions/collection.dart';
 import 'package:memri/widgets/components/shapes/circle.dart';
 import 'package:memri/widgets/empty.dart';
 import 'package:memri/cvu/widgets/renderers/renderer.dart';
@@ -78,63 +77,42 @@ class _ListRendererViewState extends RendererViewState {
     if (!viewContext.isLoaded) {
       return Empty();
     }
-    List<Widget> elements = [];
-    if (viewContext.hasItems) {
-      var lastIndex = viewContext.items.length - 1;
-      elements = List<Widget>.from(viewContext.items
-          .mapIndexed((index, item) => [
-                _buildItem(item, index),
-                if (index < lastIndex) _buildSeparator()
-              ])
-          .expand((element) => element));
-    }
 
-    if (startingElement != null) {
-      elements.insertAll(0, [
-        ListTile(
-          dense: true,
-          minVerticalPadding: 0,
-          visualDensity: VisualDensity(horizontal: -2, vertical: -2),
-          contentPadding:
-              EdgeInsets.fromLTRB(insets.left, 0, insets.right, spacing.y / 2),
-          title: startingElement!,
-        ),
-        _buildSeparator()
-      ]);
-    }
-    if (trailingElement != null) {
-      elements.addAll([
-        _buildSeparator(),
-        ListTile(
-          dense: true,
-          minVerticalPadding: 0,
-          visualDensity: VisualDensity(horizontal: -2, vertical: -2),
-          contentPadding:
-              EdgeInsets.fromLTRB(insets.left, 0, insets.right, spacing.y / 2),
-          title: trailingElement!,
-        )
-      ]);
-    }
+    var itemCount = viewContext.items.length;
 
-    return elements.isNotEmpty
-        ? ListView.custom(
-            reverse: isReverse,
+    if (startingElement != null) itemCount++;
+    if (trailingElement != null) itemCount++;
+
+    return itemCount > 0
+        ? ListView.separated(
             shrinkWrap: true,
             padding: EdgeInsets.fromLTRB(0, insets.top + 80, 0, insets.bottom),
-            childrenDelegate: SliverChildListDelegate(elements),
-
-            //TODO with large data ListView.custom will lag, should open ListView.separated and delete ListView.custom as soon as this issue is solved: https://github.com/flutter/flutter/issues/21023
-            /*child: ListView.separated(
-                    shrinkWrap: true,
-                    // physics: BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
-                    padding: EdgeInsets.fromLTRB(0, insets.top, 0, insets.bottom),
-                    itemBuilder: (context, index) {
-                      var item = viewContext.items[index];
-                      return _buildItem(item, index);
-                    },
-                    separatorBuilder: (context, index) => _buildSeparator(),
-                    itemCount: viewContext.items.length)*/
-          )
+            itemBuilder: (context, index) {
+              var item = viewContext.items[index];
+              if (index == 0 && startingElement != null) {
+                return ListTile(
+                  dense: true,
+                  minVerticalPadding: 0,
+                  visualDensity: VisualDensity(horizontal: -2, vertical: -2),
+                  contentPadding: EdgeInsets.fromLTRB(
+                      insets.left, 0, insets.right, spacing.y / 2),
+                  title: startingElement!,
+                );
+              } else if (index == itemCount - 1 && trailingElement != null) {
+                return ListTile(
+                  dense: true,
+                  minVerticalPadding: 0,
+                  visualDensity: VisualDensity(horizontal: -2, vertical: -2),
+                  contentPadding: EdgeInsets.fromLTRB(
+                      insets.left, 0, insets.right, spacing.y / 2),
+                  title: trailingElement!,
+                );
+              }
+              return _buildItem(item, index);
+            },
+            separatorBuilder: (context, index) => _buildSeparator(),
+            reverse: isReverse,
+            itemCount: itemCount)
         : emptyResult ??
             Padding(
               padding: EdgeInsets.all(30),
