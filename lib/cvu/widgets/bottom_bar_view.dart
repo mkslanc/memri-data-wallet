e6b6b1d361aeeb795815cb6396cb8281d7976a31
anijanyan <amirjanyanani@gmail.com>
2024-10-11 13:12:51 +0400

    unnecessary widget states removed

--- lib/cvu/widgets/bottom_bar_view.dart
+++ lib/cvu/widgets/bottom_bar_view.dart
@@ -10,55 +10,35 @@ import '../models/cvu_value.dart';
 import '../services/cvu_action.dart';
 import '../../widgets/components/buttons/action_button.dart';
 
-
 /// This view contains the contents of the 'Bottom bar' which is shown below renderer content.
-class BottomBarView extends StatefulWidget {
+class BottomBarView extends StatelessWidget {
   final ViewContextController viewContext;
 
   BottomBarView({required this.viewContext});
 
-  @override
-  _BottomBarViewState createState() => _BottomBarViewState();
-}
-
-class _BottomBarViewState extends State<BottomBarView> {
-  List<Widget> actionButtons = [];
-
-  @override
-  initState() {
-    super.initState();
-    actionButtons = _getActionButtons();
-  }
-
-  @override
-  void didUpdateWidget(oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    actionButtons = _getActionButtons();
-  }
-
-  List<Widget> _getActionButtons() {
+  List<Widget> get actionButtons {
     List<Widget> buttons = [];
 
     List<CVUAction> actions = [];
-    if (widget.viewContext.focusedItem != null) {
-      actions = widget.viewContext.itemPropertyResolver?.actions("filterButtons") ?? [];
+    if (viewContext.focusedItem != null) {
+      actions = viewContext.itemPropertyResolver?.actions("filterButtons") ?? [];
     }
 
     if (actions.isNotEmpty) {
       buttons = actions.map((action) {
         return ActionButton(
             action: action,
-            viewContext: widget.viewContext.getCVUContext(item: widget.viewContext.focusedItem));
+            viewContext: viewContext.getCVUContext(item: viewContext.focusedItem));
       }).toList();
     } else {
       var filterButtons =
-          widget.viewContext.viewDefinitionPropertyResolver.stringArray("filterButtons");
+          viewContext.viewDefinitionPropertyResolver.stringArray("filterButtons");
       buttons = filterButtons.compactMap((el) {
         var action = cvuAction(el);
         if (action != null) {
           return ActionButton(
               action: action(vars: <String, CVUValue>{}),
-              viewContext: widget.viewContext.getCVUContext());
+              viewContext: viewContext.getCVUContext());
         }
         return null;
       });
--- lib/screens/cvu_screen.dart
+++ lib/screens/cvu_screen.dart
@@ -2,11 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:memri/cvu/controllers/view_context_controller.dart';
 import 'package:memri/cvu/widgets/scene_content_view.dart';
 import 'package:memri/cvu/widgets/bottom_bar_view.dart';
-import 'package:provider/provider.dart';
 
-import '../providers/app_provider.dart';
 import '../widgets/scaffold/cvu_scaffold.dart';
-import 'error_connectivity_screen.dart';
 
 class CVUScreen extends StatefulWidget {
   final ViewContextController viewContextController;
@@ -18,55 +15,16 @@ class CVUScreen extends StatefulWidget {
 }
 
 class _CVUScreenState extends State<CVUScreen> {
-  late Future _init;
-
-  initState() {
-    super.initState();
-    _init = init();
-  }
-
-  init() async {
-    try {
-      await Provider.of<AppProvider>(context, listen: false).initCVUDefinitions();
-      return true;
-    } catch (error) {
-      return false;
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: _init,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            return CVUScaffold(
-            viewContextController: widget.viewContextController,
-              child: SceneContentView(
-                viewContext: widget.viewContextController,
-              ),
-            bottomBar: BottomBarView(
-              viewContext: widget.viewContextController,
-            ),
-            );
-          } else if (snapshot.hasError) {
-            return ErrorConnectivityScreen(
-              errorMessage: snapshot.error.toString(),
-              onRetry: () {
-                Navigator.pushReplacement(
-                  context,
-                  PageRouteBuilder(
-                    pageBuilder: (_, __, ___) => CVUScreen(viewContextController: widget.viewContextController,),
-                    transitionDuration: Duration.zero,
-                    reverseTransitionDuration: Duration.zero,
-                  ),
-                );
-              },
-            );
-          } else {
-            return const Center(child: CircularProgressIndicator());
-          }
-      },
+    return CVUScaffold(
+      viewContextController: widget.viewContextController,
+      child: SceneContentView(
+        viewContext: widget.viewContextController,
+      ),
+      bottomBar: BottomBarView(
+        viewContext: widget.viewContextController,
+      ),
     );
   }
 }
